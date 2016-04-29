0b8f081c3834330891eece2af6bff49485d0b364
Giovani Guizzo <giovaniguizzo@gmail.com>
2016-04-29 10:22:32 -0300

    Corrigido o HookEvaluate tirando o MAX_MIN.txt

--- src/org/uma/jmetal/irace/HookEvaluate.java
+++ src/org/uma/jmetal/irace/HookEvaluate.java
@@ -2,13 +2,10 @@ package org.uma.jmetal.irace;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.uma.jmetal.main.ComputeHypervolumeCommandLine;
 import org.uma.jmetal.measure.qualityindicator.HypervolumeCalculator;
 
@@ -28,35 +25,19 @@ public class HookEvaluate {
 
         HypervolumeCalculator calculator = new HypervolumeCalculator();
         File dir = new File("./");
-        for (File file : dir.listFiles((dirrectory, name) -> name.startsWith("FUN_") || name.equals("MAX_MIN.txt"))) {
+        for (File file : dir.listFiles((dirrectory, name) -> name.startsWith("FUN_"))) {
             calculator.addParetoFront(file.getPath());
         }
 
-        try (FileWriter maxMinWriter = new FileWriter("MAX_MIN.txt")) {
-            newArgs.add("--max");
-            Arrays.stream(calculator.getMaximumValues())
-                    .forEach(doubleValue -> {
-                        newArgs.add(String.valueOf(doubleValue));
-                        try {
-                            maxMinWriter.write(String.valueOf(doubleValue) + " ");
-                        } catch (IOException ex) {
-                            Logger.getLogger(HookEvaluate.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    });
-            maxMinWriter.write("\n");
+        newArgs.add("--max");
+        Arrays.stream(calculator.getMaximumValues())
+                .forEach(doubleValue
+                        -> newArgs.add(String.valueOf(doubleValue)));
 
-            newArgs.add("--min");
-            Arrays.stream(calculator.getMinimumValues())
-                    .forEach(doubleValue -> {
-                        newArgs.add(String.valueOf(doubleValue));
-                        try {
-                            maxMinWriter.write(String.valueOf(doubleValue) + " ");
-                        } catch (IOException ex) {
-                            Logger.getLogger(HookEvaluate.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    });
-            maxMinWriter.write("\n");
-        }
+        newArgs.add("--min");
+        Arrays.stream(calculator.getMinimumValues())
+                .forEach(doubleValue
+                        -> newArgs.add(String.valueOf(doubleValue)));
 
         return newArgs.toArray(new String[newArgs.size()]);
     }
